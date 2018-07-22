b326f742d5674925510c821fec7fe09d90af477c
Ian Winter <ianw@protonmail.com>
2018-07-21 20:11:53 -0700

    reorganized a lot

--- weblife.js
+++ weblife.js
@@ -1,136 +1,155 @@
-var grid = {
-    cols : 10,
-    rows : 10,
-    rules : [[]],
-    cells : [[]],
-    init : function() {
-    },
-    step : function() {
-    },
-    genSoup : function(rows, cols, density) {
-    },
+function ruleSet (ruleString) {
+    this.ruleArray = this.parseRuleString(ruleString);
 
-    update: function(input) {
-        split = input.split(" ");
-        cols = parseInt(split[0]);
-        rows = parseInt(split[1]);
-        cells = split[2].trim();
-        if (this.cells.length != this.cols * this.rows) {
-            error("grid.update: Invalid map!");
-            return "error";
-        }
-        this.cols = cols;
-        this.rows = rows;
-        this.cells = cells;
-    },
+    parseRuleString(ruleString) {
+        // Confirm correct format, fail if its wrong
+        // return array
+    }
+
 }
 
-var display = {
-    cellSize : 30,
-    canvas : 0,
-    ctx : 0,
-    top : 0,
-    left : 0,
-    init : function() {
-        display.canvas = document.getElementById("theGrid");
-        display.ctx = display.canvas.getContext("2d");
+function dimentions (cols, rows) {
+    this.dimentions = [cols, rows];
+}
+
+function engine (cols, rows, ruleString) {
+    this.cols = cols;
+    this.rows = rows;
+    this.rules = new RuleSet(ruleString);
+    this.cells = Array(cols * rows);
+
+    step: function () {
+        // TODO
+        // this function should call the necessary functions to deal with
+        // stepping the grid from one state to the next
     },
-    resize : function() {
-        var maxHeight = 0.5 * window.innerHeight;
-        var maxWidth = 0.7 * window.innerWidth;
-        if ((grid.cols/grid.rows) > (maxWidth/maxHeight)) {
+
+    generateSoup: function (density) {
+        // TODO
+        // should iterate thru cell array and fill a square with density
+    },
+
+    getCells: function() {
+        return this.cells;
+    }
+
+    getCols: function() {
+        return this.cols;
+    }
+
+
+}
+
+function display (cols, rows, cellSize) {
+
+    this.cellSize = cellSize;
+    this.jtop = 0;
+    this.left = 0;
+    this.canvas = document.getElementById("theGrid");
+    this.ctx = this.canvas.getContext("2d");
+
+    resize(cols, rows);
+
+    resize: function (cols, rows) {
+        let maxHeight = 0.5 * window.innerHeight;
             this.canvas.width = maxWidth;
-            this.canvas.height = (grid.rows/grid.cols) * maxWidth;
+            this.canvas.height = (grid.rows / grid.cols) * maxWidth;
             this.cellSize = this.canvas.width / grid.cols;
         } else {
             this.canvas.height = maxHeight;
-            this.canvas.width = (grid.cols/grid.rows) * maxHeight ;
-            this.cellSize = this.canvas.height / grid.rows;
+            this.gridDisplay.width = (grid.cols / grid.rows) * maxHeight;
+            this.cellSize = this.gridDisplay.height / grid.rows;
         }
-        this.top = this.canvas.offsetTop;
-        this.left = this.canvas.offsetLeft;
+        this.top = this.gridDisplay.offsetTop;
+        this.left = this.gridDisplay.offsetLeft;
     },
-    update : function() {
-        display.init();
+
+    update: function () {
+        gridDisplay.init();
         var xpos = 0, ypos = 0;
         this.ctx.strokeStyle = "#E0E0E0";
         for (i = 0; i < grid.cols * grid.rows; i++) {
-            if (i % grid.cols == 0 && i != 0) {
+            if (i % grid.cols === 0 && i !== 0) {
                 ypos += this.cellSize;
                 xpos = 0;
             }
-            if (grid.cells[i] == '1') {
-                this.ctx.fillRect(xpos, ypos, this.cellSize, this.cellSize)
+            if (grid.cells[i] === "1") {
+                this.ctx.fillRect(xpos, ypos, this.cellSize, this.cellSize);
             }
             this.ctx.strokeRect(xpos, ypos, this.cellSize, this.cellSize);
-            xpos += this.cellSize
+            xpos += this.cellSize;
         }
     },
-}
 
-var ui = {
-    interval : undefined;
-    init : function() {
-    },
-    step : function() {
-    },
-    play : function() {
-        clearInterval(interval);
-        interval = setInterval(step, 100);
-        document.getElementById("playStatus").innerHTML = "&#9654;"
-    },
-    pause : function() {
-        clearInterval(interval);
-        document.getElementById("playStatus").innerHTML = "&#9646;&#9646;"
-    },
 };
 
+let ui = {
+    interval: undefined;
 
-function init() {
-    window.onresize = display.update()
-    display.canvas.addEventListener('click', function toggle(event) {
-        console.log(event.pageX)
-        var x = ftoi((event.pageX - display.left)/display.cellSize)
-        var y = ftoi((event.pageY - display.top)/display.cellSize)
-        var i = y*grid.cols + x
-        console.log("x: " + String(x) + " y: " + String(x))
-        console.log(grid.cells[i])
-        if (grid.cells[i] == '1') {
-            grid.cells = strReplace(grid.cells, i, '0');
-        } else {
-            grid.cells = strReplace(grid.cells, i, '1');
-        }
-        console.log(grid.cells)
-        display.update(grid);
-    }, false);
-    display.update(grid);
+    function sendError(input) {
+        document.getElementById("status").innerHTML = input;
+        console.log(input);
+    }
+    init : function () {
+    };
+    ,
+    step : function () {
+    }
+    ,
+    let interval;
+    play : function () {
+        clearInterval(interval);
+        interval = setInterval(step, 100);
+        document.getElementById("playStatus").innerHTML = "&#9654;";
+    }
+    ,
+    pause : function () {
+        clearInterval(interval);
+        document.getElementById("playStatus").innerHTML = "&#9646;&#9646;";
+    }
+
+    updateRules : function () {
+        rules = document.getElementById("rulesInput").value;
+        // TODO: Finish
+    }
 }
 
-var interval;
+// Gets called on page load
+function init() {
+    window.onresize = gridDisplay.update();
+    gridDisplay.canvas.addEventListener("click", function toggle(event) {
+        let x = ftoi((event.pageX - gridDisplay.left) / gridDisplay.cellSize);
+        let y = ftoi((event.pageY - gridDisplay.top) / gridDisplay.cellSize);
+        let i = y * grid.cols + x;
+        if (grid.cells[i] === "1") {
+            grid.cells = strReplace(grid.cells, i, "0");
+        } else {
+            grid.cells = strReplace(grid.cells, i, "1");
+        }
+        console.log(grid.cells);
+        gridDisplay.update(grid);
+    }, false);
+    gridDisplay.update(grid);
+}
+
 function pause() {
 }
 
-function error(input) {
-    document.getElementById("status").innerHTML = input;
-    console.log(input);
-}
 
 function ftoi(x) {
-    return x | 0
+    return x | 0;
 }
 
 function loadBitmap() {
     pbm = mapToPbm(document.getElementById("gridInput").value);
-    if (pbm != "error") {
-        console.log(grid.update(pbm))
-    if (grid.update(pbm) != "error")
-        display.init();
-        display.update(grid);
+    if (pbm !== "error") {
+        console.log(grid.update(pbm));
+        if (grid.update(pbm) !== "error")
+            gridDisplay.init();
+        gridDisplay.update(grid);
     }
 }
 
 function updRules() {
-    error("");
-    rules = document.getElementById("rulesInput").value;
-    regexp = /^\s*b[0-8]+\/s[0-8]+\s*$/i;
 }
+
