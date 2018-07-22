98f26b2d78fc440eedc0e029536e70c1aa953ec4
Ian Winter <ianw@protonmail.com>
2018-07-22 00:46:59 -0700

    Started work

--- weblife.js
+++ weblife.js
@@ -1,21 +1,15 @@
-var request = { 
-	"Grid" : "",
-	"Rules" : "b3/s23",
-        "Density" : "0.5",
-        "Width" : "10",
-        "Height" : "10",
-        "New" : "0",
-}
-
-var response = {
-    Pbm : "",
-    Error: "",
-}
-
 var grid = {
-    cols : 12,
+    cols : 10,
     rows : 10,
-    cells : "000000000000000000000000000100000000000010000000001110000000000000000000000000000000000000000000000000000000000000000000",
+    rules : [[]],
+    cells : [[]],
+    init : function() {
+    },
+    step : function() {
+    },
+    genSoup : function(rows, cols, density) {
+    },
+
     update: function(input) {
         split = input.split(" ");
         cols = parseInt(split[0]);
@@ -38,6 +32,10 @@ var display = {
     top : 0,
     left : 0,
     init : function() {
+        display.canvas = document.getElementById("theGrid");
+        display.ctx = display.canvas.getContext("2d");
+    },
+    resize : function() {
         var maxHeight = 0.5 * window.innerHeight;
         var maxWidth = 0.7 * window.innerWidth;
         if ((grid.cols/grid.rows) > (maxWidth/maxHeight)) {
@@ -51,8 +49,6 @@ var display = {
         }
         this.top = this.canvas.offsetTop;
         this.left = this.canvas.offsetLeft;
-        console.log(this.top)
-        console.log(this.left)
     },
     update : function() {
         display.init();
@@ -72,11 +68,25 @@ var display = {
     },
 }
 
+var ui = {
+    interval : undefined;
+    init : function() {
+    },
+    step : function() {
+    },
+    play : function() {
+        clearInterval(interval);
+        interval = setInterval(step, 100);
+        document.getElementById("playStatus").innerHTML = "&#9654;"
+    },
+    pause : function() {
+        clearInterval(interval);
+        document.getElementById("playStatus").innerHTML = "&#9646;&#9646;"
+    },
+};
+
 
 function init() {
-    display.canvas = document.getElementById("theGrid");
-    display.ctx = display.canvas.getContext("2d");
-    document.getElementById("gridInput").innerText = "000000000000\n000000000000\n000100000000\n000010000000\n001110000000\n000000000000\n000000000000\n000000000000\n000000000000\n000000000000\n";
     window.onresize = display.update()
     display.canvas.addEventListener('click', function toggle(event) {
         console.log(event.pageX)
@@ -97,29 +107,7 @@ function init() {
 }
 
 var interval;
-function play() {
-    clearInterval(interval);
-    interval = setInterval(step, 100);
-    document.getElementById("playStatus").innerHTML = "&#9654;"
-    
-}
-
-
-function sendRequest() {
-    json = JSON.stringify(request);
-    console.log(json);
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            response.Pbm = this.responseText;
-            console.log("Reply:" + this.responseText)
-            grid.update(response.Pbm);
-            display.update(grid);
-        }
-    }
-    xhttp.open("POST", "relay.cgi", true);
-    xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send(json);
+function pause() {
 }
 
 function error(input) {
@@ -129,33 +117,6 @@ function error(input) {
 
 function ftoi(x) {
     return x | 0
-}
-
-// Replace character i in string s with character c
-function strReplace(s, i, c) {
-    return s.substr(0, i) + c + s.substr(i+1);
-}
-
-function mapToPbm(map) {
-    var i, cols = 0
-    var grid = "";
-    map = map.trim()
-    for (i = 0; i < map.length; i++) {
-        if (map[i] != '\n') {
-            grid = grid + map[i]
-        } else {
-            if (cols == 0) {
-                cols = i;
-            }
-        }
-    }
-    grid = grid.trim();
-    var rows = ftoi(grid.length/cols);
-    if (rows * cols != grid.length) {
-        error("Invalid map!");
-        return "error";
-    }
-    return String(cols) + " " + String(rows) + " " + grid
 }
 
 function loadBitmap() {
@@ -168,32 +129,8 @@ function loadBitmap() {
     }
 }
 
-function soup() {
-    request.Width = document.getElementById("randW").value;
-    request.Height = document.getElementById("randH").value;
-    request.Density = document.getElementById("randD").value;
-    request.New = "1";
-    sendRequest();
-    request.New = "0";
-}
-
 function updRules() {
     error("");
     rules = document.getElementById("rulesInput").value;
     regexp = /^\s*b[0-8]+\/s[0-8]+\s*$/i;
-    if (regexp.test(rules)) {
-	request.Rules = document.getElementById("rulesInput").value;
-    } else {
-        error("Invalid rulestring!");
-    }
-}
-
-function pause() {
-    clearInterval(interval);
-    document.getElementById("playStatus").innerHTML = "&#9646;&#9646;"
-}
-
-function step() {
-    request.Grid = String(grid.cols) + " " + String(grid.rows) + " " + String(grid.cells);
-    sendRequest();
 }
